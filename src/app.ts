d84854bc1d3f9e36d932ec720aede557500d319d
Vanderllee <vanderllee@gmail.com>
2021-10-19 19:32:12 -0300

    added access github code

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
+import "dotenv/config";
 import * as express from 'express';
+const port = 4000;
 
 const app = express();
 
-app.listen(4000, () => console.log('Server running on port 4000'));
+
+app.get('/github', (req, res) => {
+
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+
+   
+})
+
+app.get('/signin/callback', (req, res) => {
+    const { code } = req.query;
+
+    res.send(code);
+})
+
+
+app.listen(port, () => console.log(`Server is running on port ${ port }`));
