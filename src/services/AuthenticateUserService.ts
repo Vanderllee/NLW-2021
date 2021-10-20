d2ee3fa0aac3f970298f910e566d2bbdf00008f8
Vanderllee <vanderllee@gmail.com>
2021-10-19 21:23:42 -0300

    added get dataUser from github

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 
+type AccessTokenResponse = {
+    access_token: string
+}
+
+type UserResponse = {
+    avatar_url: string;
+    login: string;
+    id: number;
+    name: string;
+}
+
 class AuthenticateUserService {
 
     async execute(code: string) {
         const url = 'https://github.com/login/oauth/access_token';
 
-        const response = await axios.post(url, null, {
+        const { data: accessTokenResponse } = await axios.post<AccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -13,6 +24,12 @@ class AuthenticateUserService {
             },
             headers: {
                 "Accept": "application/json"
+            }
+        })
+
+        const response = await axios.get<UserResponse>('https://api.github.com/user', {
+            headers: {
+                authorization: `Bearer ${ accessTokenResponse.access_token }`
             }
         })
 
