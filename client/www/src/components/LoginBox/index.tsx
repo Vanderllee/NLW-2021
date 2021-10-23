ce4c2bd1a4f02b08151e6ac608755c98198df81e
Vanderllee <vanderllee@gmail.com>
2021-10-22 22:20:05 -0300

    update LoginBox styles

--- client/www/src/components/LoginBox/index.tsx
+++ client/www/src/components/LoginBox/index.tsx
@@ -1,6 +1,18 @@
+import { VscGithubInverted } from 'react-icons/vsc'
+
+import styles from './styles.module.scss';
 
 export function LoginBox() {
     return (
-        <h1>LoginBox</h1>
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe suas mensagens</strong>
+            <a 
+                href="#" 
+                className={styles.signinWithGithub}
+            >
+                <VscGithubInverted size="24"/>
+                Entrar com Github
+            </a>
+        </div>
     )
 }
