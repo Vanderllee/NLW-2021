efac02b0ecfb82f831c6ad4cc3b3e33d2f51ca0d
Vanderllee <vanderllee@gmail.com>
2021-10-23 16:14:37 -0300

    added styles and responsividade

--- client/www/src/components/MessageList/index.tsx
+++ client/www/src/components/MessageList/index.tsx
@@ -1,7 +1,54 @@
 
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
 
 export function MessageList() {
     return (
-        <h1>MessageList</h1>
+        
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Eu estou aprendendo muito com a nlw Heat!
+                    </p>
+
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vanderllee.png" alt="Vanderllee" />
+                        </div>
+                        <span>Vanderllee</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Eu estou aprendendo muito com a nlw Heat!
+                    </p>
+
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vanderllee.png" alt="Vanderllee" />
+                        </div>
+                        <span>Vanderllee</span>
+                    </div>
+                </li>
+
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Eu estou aprendendo muito com a nlw Heat!
+                    </p>
+
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/vanderllee.png" alt="Vanderllee" />
+                        </div>
+                        <span>Vanderllee</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
     )
 }
